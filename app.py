bb5fa51337f4e1e73500596dc6b26ff056fe1ffc
FoxntaU <140737499+FoxntaU@users.noreply.github.com>
2024-05-10 07:09:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -16,11 +17,14 @@ def factorial(numero):
 
 @app.route('/<id>')
 def calculate(id):
+    container_id = os.uname()[1] 
     factorial_value = factorial(int(id))
     if isinstance(factorial_value, int):
-        return jsonify({'factorial': factorial_value})
+        return jsonify({'factorial': factorial_value
+                        ,'container_id': container_id})
     else:
-        return jsonify({'error': factorial_value})
+        return jsonify({'error': factorial_value,
+                        'container_id': container_id})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
